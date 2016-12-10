2e0408f1c2f00ad4db430a26b5920c93b2b42584
Schwarze Katze <derschwarzekatze@gmail.com>
2016-12-10 14:21:21 -0500

    Not needed.

--- HelloCropit/src/test/java/com/hellocropit/constants/ControllerConstants.java
+++
@@ -1,8 +0,0 @@
-package com.hellocropit.constants;
-
-public class ControllerConstants {
-	
-	public static final String FileController = "FileController";
-	public static final String ProfileController = "ProfileController";
-
-}
--- HelloCropit/src/test/java/com/hellocropit/constants/Pages.java
+++
@@ -1,8 +0,0 @@
-package com.hellocropit.constants;
-
-public class Pages {
-	
-	public static final String PROFILE_PAGE = "profile";
-	public static final String CROPIT_PAGE = "cropit";
-	
-}
