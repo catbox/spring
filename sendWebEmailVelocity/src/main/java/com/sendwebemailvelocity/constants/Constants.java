47d7b41fc0b1d60147e3c01ec35b054a383d7bfe
Schwarze Katze <derschwarzekatze@gmail.com>
2017-01-28 00:09:51 -0500

    Constants.

---
+++ sendWebEmailVelocity/src/main/java/com/sendwebemailvelocity/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.sendwebemailvelocity.constants;
+
+public class Constants {
+	
+	public static final String EMPTY_STRING = "";
+	public static final String FIRST_NAME_FIELD = "emailForm.firstName";
+	public static final String LAST_NAME_FIELD = "emailForm.lastName";
+	public static final String EMAIL_ADDRESS_FIELD = "emailForm.emailAddress";
+
+}
