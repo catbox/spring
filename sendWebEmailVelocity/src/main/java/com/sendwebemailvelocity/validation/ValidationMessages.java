d5da3a14519ec791caad616e2ea4cb57901a76c7
Schwarze Katze <derschwarzekatze@gmail.com>
2017-02-03 21:22:48 -0500

    Messages.

--- sendWebEmailVelocity/src/main/java/com/sendwebemailvelocity/validation/ValidationMessages.java
+++ sendWebEmailVelocity/src/main/java/com/sendwebemailvelocity/validation/ValidationMessages.java
@@ -5,5 +5,7 @@ public class ValidationMessages {
 	public static final String FIRST_NAME_REQUIRED_MESSAGE = "NotEmpty.firstName";
 	public static final String LAST_NAME_REQUIRED_MESSAGE = "NotEmpty.lastName";
 	public static final String EMAIL_ADDRESS_REQUIRED_MESSAGE = "NotEmpty.emailAddress";
+	public static final String EMAIL_SENT_SUCCESS = "EMAIL_SENT_SUCCESS";
+	public static final String EMAIL_SENT_FAILED = "EMAIL_SENT_FAILED";
 
 }
