aa8c9b1b405b047584dec69822391d8182f1e9cb
Schwarze Katze <derschwarzekatze@gmail.com>
2017-02-03 21:22:05 -0500

    Send an email.

---
+++ sendWebEmailVelocity/src/main/java/com/sendwebemailvelocity/service/SendEmail.java
@@ -0,0 +1,13 @@
+package com.sendwebemailvelocity.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("sendMailService")
+public class SendEmail {
+	
+	public boolean mailSender(){
+	
+		return false;
+	}
+
+}
