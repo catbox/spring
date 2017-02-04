895492b1bb457a11e7e64c2374a420cbb65c2d0a
Schwarze Katze <derschwarzekatze@gmail.com>
2017-02-03 21:23:27 -0500

    Constant - EMAIL_SENT

--- sendWebEmailVelocity/src/main/java/com/sendwebemailvelocity/constants/ValidationConstants.java
+++ sendWebEmailVelocity/src/main/java/com/sendwebemailvelocity/constants/ValidationConstants.java
@@ -3,5 +3,6 @@ package com.sendwebemailvelocity.constants;
 public class ValidationConstants {
 	
 	public static final String FIELD_ERRORS = "fieldErrors";
+	public static final String EMAIL_SENT = "emailSent";
 
 }
