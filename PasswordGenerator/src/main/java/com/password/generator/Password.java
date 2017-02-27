e642fed21ccabcdecbe6fc273384662301737d00
Schwarze Katze <derschwarzekatze@gmail.com>
2017-02-26 23:27:12 -0500

    1) Digits 2) Lower Case 3) Upper Case 4) Symbols.

--- PasswordGenerator/src/main/java/com/password/generator/Password.java
+++ PasswordGenerator/src/main/java/com/password/generator/Password.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Password {
 	
-	String[] passwordCharacters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
-								   "u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N",
-								   "O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7",
-								   "8","9","!","@","£","$","%","^","&","*","~","`","<",">","/","?","|","[","]","+",
-								   "=","-","_"};
+	String[] passwordCharactersAndDigits = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
+											"v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+											"Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"};
+	
+	String[] passwordSymbols = {"$","#","%","@"};
 
 	public Password() {
 		// Do nothing
@@ -16,16 +16,37 @@ public class Password {
 	
 	public String generatePassword(){
 
-		StringBuffer password = new StringBuffer();
-		int passwordLength = 10;
+		// Characters and digits elements.
+		StringBuffer pwdCharsAndDigits = new StringBuffer();
+		int pwdLength = 14;
 
-		for(int i = 0; i < passwordLength; i++){			
+		for(int i = 0; i < pwdLength; i++){			
 			Random r = new Random();
-			int random = r.nextInt(passwordCharacters.length);		
-			password.append(passwordCharacters[random]);	
+			int random = r.nextInt(passwordCharactersAndDigits.length);		
+			pwdCharsAndDigits.append(passwordCharactersAndDigits[random]);	
 		}
+		
+		String passwordCharsAndDigits = pwdCharsAndDigits.toString();
+		
+		// Symbols.
+		StringBuffer pwdSymbol = new StringBuffer();
+		int pwdSymbolLength = 1;
+		
+		for(int i = 0; i < pwdSymbolLength; i++){			
+			Random r = new Random();
+			int random = r.nextInt(passwordSymbols.length);		
+			pwdSymbol.append(passwordSymbols[random]);	
+		}
+		
+		String passwordSymbol = pwdSymbol.toString();;
 
-		return password.toString();		
+		// Complete password.
+		StringBuilder pwdBuilder = new StringBuilder();
+		pwdBuilder.append(passwordSymbol).append(passwordCharsAndDigits);
+		
+		String password = pwdBuilder.toString();
+		
+		return password;
 	}
 
 }
